29f7b0eb14859409613e567b9d8a629b0ea8402f
LucasDel123 <lucas.delliste@hotmail.fr>
2024-11-21 16:31:03 +0100

    header.h

--- header.h
+++ header.h
@@ -1,7 +1,3 @@
-//
-// Created by Audran on 19/11/2024.
-//
-
 #ifndef HEADER_H
 #define HEADER_H
 
@@ -19,7 +15,10 @@
 
 void deplacerPion(char plateau[9][9], int *X, int *Y, char direction);
 void deplacerPion2(char plateau[9][9], int *XX, int *YY, char direction);
+void deplacerPion3(char plateau[9][9], int *XXX, int *YYY, char direction);
+void deplacerPion4(char plateau[9][9], int *XXXX, int *YYYY, char direction);
 void initialiserPlateau(char plateau[9][9], int X, int Y, int XX, int YY);
+void initialiserPlateau4(char plateau[9][9], int X, int Y, int XX, int YY, int XXX, int YYY, int XXXX, int YYYY);
 void afficherPlateau(char plateau[9][9]);
 void demanderNombreDeJoueurs(int *nombreJoueurs);
 void demanderNomsJoueurs(char nomsJoueurs[MAX_JOUEURS][25], int nombreJoueurs);
