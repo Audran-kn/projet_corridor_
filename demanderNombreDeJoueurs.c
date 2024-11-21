6cf73ae5efc460a68d9cf30daba3052f6b0f7374
Audran <audran.chevalier@edu.ece.fr>
2024-11-21 11:12:03 +0100

    update nb joueurs V3

--- demanderNombreDeJoueurs.c
+++ demanderNombreDeJoueurs.c
@@ -3,12 +3,12 @@
 //
 #include "header.h"
 
-void demanderNombreDeJoueurs(int *nombreJoueurs) {  // Passage par référence
+void demanderNombreDeJoueurs(int *nombreJoueurs) {
     do {
         printf("Combien de personnes jouent ? (%d, %d) : ", MIN_JOUEURS, MAX_JOUEURS);
         scanf("%d", nombreJoueurs);  // Utilisation du pointeur pour modifier nombreJoueurs
-        if (*nombreJoueurs < MIN_JOUEURS || *nombreJoueurs > MAX_JOUEURS) {
+        if (*nombreJoueurs != MIN_JOUEURS && *nombreJoueurs != MAX_JOUEURS) {
             printf("Nombre de joueurs invalide. Veuillez reessayer.\n");
         }
-    } while (*nombreJoueurs != MIN_JOUEURS || *nombreJoueurs != MAX_JOUEURS);
+    } while (*nombreJoueurs != MIN_JOUEURS && *nombreJoueurs != MAX_JOUEURS);
 }
