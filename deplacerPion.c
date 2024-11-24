7160d4a404fb8354fd541626c6ba073c0d50b185
LucasDel123 <lucas.delliste@hotmail.fr>
2024-11-24 20:12:18 +0100

    deplacerPion version finale.c

--- deplacerPion.c
+++ deplacerPion.c
@@ -1,18 +1,25 @@
-//
-// Created by Audran on 19/11/2024.
-//
 #include "header.h"
 
-void deplacerPion(char plateau[9][9], int *X, int *Y, char direction) {
-    // Supprimer la position actuelle du pion
+void deplacerPion(char plateau[TAILLE][TAILLE], int *X, int *Y, char direction, int *XX, int *YY) {
+
     plateau[*X][*Y] = ' '; // On remet la case à vide
+
 
     // Calcul de la nouvelle position selon la direction
     if (direction == '1' && *X > 0) (*X)--;           // Haut
-    else if (direction == '2' && *X < 9 - 1) (*X)++;  // Bas
+    else if (direction == '2' && *X < TAILLE - 1) (*X)++;  // Bas
     else if (direction == '3' && *Y > 0) (*Y)--;      // Gauche
-    else if (direction == '4' && *Y < 9 - 1) (*Y)++;  // Droite
+    else if (direction == '4' && *Y < TAILLE - 1) (*Y)++;  // Droite
     else if (direction == '0') (*Y)+0, (*X)+0;
 
+    if (*X == *XX && *Y == *YY && direction == '1') {
+        (*X)--; // Saut dans la même direction
+    } else if (*X == *XX && *Y == *YY && direction == '2') {
+        (*X)++;
+    } else if (*X == *XX && *Y == *YY && direction == '3') {
+        (*Y)--;
+    } else if (*X == *XX && *Y == *YY && direction == '4') {
+        (*Y)++;
+    }
     plateau[*X][*Y] = 0x03;
 }
