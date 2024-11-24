71ce709e1d2da24c8c8089f869cda996e157d65d
LucasDel123 <lucas.delliste@hotmail.fr>
2024-11-24 20:12:58 +0100

    deplacerPion2 version finale.c

--- deplacerPion2.c
+++ deplacerPion2.c
@@ -1,15 +1,21 @@
-//
-// Created by Audran on 19/11/2024.
-//
-# include "header.h"
+#include "header.h"
 
-void deplacerPion2(char plateau[9][9], int *XX, int *YY, char direction) {
+void deplacerPion2(char plateau[TAILLE][TAILLE], int *XX, int *YY, char direction, int *X, int *Y) {
     plateau[*XX][*YY] = ' ';
     if (direction == '1' && *XX > 0) (*XX)--;           // bas
-    else if (direction == '2' && *XX < 9 - 1) (*XX)++;  // haut
+    else if (direction == '2' && *XX < TAILLE - 1) (*XX)++;  // haut
     else if (direction == '3' && *YY > 0) (*YY)--;      // droite
-    else if (direction == '4' && *YY < 9 - 1) (*YY)++;  // gauche
+    else if (direction == '4' && *YY < TAILLE - 1) (*YY)++;  // gauche
     else if (direction == '0') (*YY)+0, (*XX)+0;
-    
+
+    if (*X == *XX && *Y == *YY && direction == '1') {
+        (*XX)--; // Saut dans la même direction
+    } else if (*X == *XX && *Y == *YY && direction == '2') {
+        (*XX)++;
+    } else if (*X == *XX && *Y == *YY && direction == '3') {
+        (*YY)--;
+    } else if (*X == *XX && *Y == *YY && direction == '4') {
+        (*YY)++;
+    }
     plateau[*XX][*YY] = 0x04;
 }
